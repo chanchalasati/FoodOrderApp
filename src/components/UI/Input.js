f0c116edb951236bb80565fcc08fa7f90b5898fc
chanchalasati <chanchalasati12@gmail.com>
2021-09-21 22:13:22 +0530

    component for foodOrderApp

---
+++ src/components/UI/Input.js
@@ -0,0 +1,14 @@
+
+import classes from './Input.module.css';
+const Input = (props) => {
+    
+    return (<div className={classes.input}>
+        <label htmlfor={props.input.id}>{props.label}</label>
+        <input
+            id={props.input.id}
+            {...props.input}
+        />
+    </div>);
+}
+
+export default Input;
